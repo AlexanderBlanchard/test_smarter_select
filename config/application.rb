8b538eb05fc2d73fdad001c5caf1708443c29762
Alexander Gonzalez Blanchard <alexmgb3@gmail.com>
2024-02-13 17:01:12 -0600

    Config files

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module MoviesProject
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_paths += %W[#{config.root}/app/use_cases #{config.root}/app/queries]
 
     # Configuration for the application, engines, and railties goes here.
     #
